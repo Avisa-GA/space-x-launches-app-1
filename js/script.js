5337f7c95a9fedc525cfd24dbe4137d2e5e03571
Daniel JS <daniel@mydeveloperjourney.com>
2021-05-21 11:54:00 -0500

    wip: render data to the dom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
+// Pseudocode
+
+// 1) Page Loads - Fetch data from SpaceX API
+// 2) Store resulting data in a list (Array) so we can loop over and create the UI
+// 3) Using the data to create UI and append to the DOM
+
+
 /*----- constants -----*/
+const BASE_URL = 'https://api.spacexdata.com/v3/launches';
 /*----- app's state (variables) -----*/
+let launches;
 /*----- cached element references -----*/
+const $main = $('main');
+
 /*----- event listeners -----*/
+
 /*----- functions -----*/
+
+getAppData();
+
+function getAppData() {
+    $.ajax(`${BASE_URL}?limit=9&order=desc`)
+    .then(function(data) { 
+        launches = data;
+        render();
+    });
+}
+
+
+ function render() {
+     const html = launches.map(function(launch) {
+         return `
+            <article>
+                <h2>${launch.mission_name}</h2>
+                <p>${launch.launch_year}</p>
+            </article>
+         `;
+     });
+
+     $main.html(html);
+ }
+
