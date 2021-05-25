71c4f3ec0493d0a10b3dd857720eefde76c9c5f9
Daniel JS <daniel@mydeveloperjourney.com>
2021-05-25 12:26:10 -0500

    fix: removes console.log and adds IIFE

--- js/script.js
+++ js/script.js
@@ -4,67 +4,65 @@
 // 2) Store resulting data in a list (Array) so we can loop over and create the UI
 // 3) Using the data to create UI and append to the DOM
 
+$(function () {
+    /*----- constants -----*/
+    const BASE_URL = 'https://api.spacexdata.com/v3/launches';
+    /*----- app's state (variables) -----*/
+    let launches;
+    /*----- cached element references -----*/
+    const $main = $('main');
+    const $modal = $('.modal');
 
-/*----- constants -----*/
-const BASE_URL = 'https://api.spacexdata.com/v3/launches';
-/*----- app's state (variables) -----*/
-let launches;
-/*----- cached element references -----*/
-const $main = $('main');
-const $modal = $('.modal');
+    /*----- event listeners -----*/
+    $main.on('click', 'article', handleClick);
+    /*----- functions -----*/
 
-/*----- event listeners -----*/
-$main.on('click', 'article', handleClick);
-/*----- functions -----*/
+    getAppData();
 
-getAppData();
+    function getAppData() {
+        $.ajax(`${BASE_URL}?limit=9&order=desc`)
+            .then(function (data) {
+                launches = data;
+                render();
+            });
+    }
 
-function getAppData() {
-    $.ajax(`${BASE_URL}?limit=9&order=desc`)
-    .then(function(data) { 
-        launches = data;
-        render();
-    });
-}
-
-function handleClick() {
-    const missionName = this.dataset.missionName;
-    const defaultPatch = 'https://www.socialbakers.com/www/storage/www/reports/2019-03-01/twitter/34743251.jpg';
-
-
-    const launch = launches.find(function(l) { 
-        return l.mission_name === missionName
-    });
-
-    console.log(launch);
-
-    launch.links.mission_patch_small = launch.links.mission_patch_small 
-    ? launch.links.mission_patch_small : defaultPatch;
-
-    const html = `
-    <div class="content-wrapper">
-        <div>
-            <h2>${launch.mission_name}</h2>
-            <p>${launch.details ? launch.details.slice(0, 251) + '...' : 'Details Unavailable'}</p>
-            <p><a href="${launch.links.reddit_campaign}" target="_blank">Click Here</a> for Reddit Campaign</p>
-        </div>
-        ${launch.links.mission_patch_small ? `<img src="${launch.links.mission_patch_small}" alt="${launch.mission_name}" />` : ''}
-    </div>
-    `;
-    $modal.html(html).modal();
-}
+    function handleClick() {
+        const missionName = this.dataset.missionName;
+        const defaultPatch = 'https://www.socialbakers.com/www/storage/www/reports/2019-03-01/twitter/34743251.jpg';
 
 
- function render() {
-     const html = launches.map(function(launch) {
-         return `
-            <article data-mission-name="${launch.mission_name}">
+        const launch = launches.find(function (l) {
+            return l.mission_name === missionName;
+        });
+
+        launch.links.mission_patch_small = launch.links.mission_patch_small ?
+            launch.links.mission_patch_small : defaultPatch;
+
+        const html = `
+        <div class="content-wrapper">
+            <div>
                 <h2>${launch.mission_name}</h2>
-                <p>${launch.launch_year}</p>
-            </article>
-         `;
-     });
+                <p>${launch.details ? launch.details.slice(0, 251) + '...' : 'Details Unavailable'}</p>
+                <p><a href="${launch.links.reddit_campaign}" target="_blank">Click Here</a> for Reddit Campaign</p>
+            </div>
+            ${launch.links.mission_patch_small ? `<img src="${launch.links.mission_patch_small}" alt="${launch.mission_name}" />` : ''}
+        </div>
+        `;
+        $modal.html(html).modal();
+    }
 
-     $main.html(html);
- }
 
+    function render() {
+        const html = launches.map(function (launch) {
+            return `
+                <article data-mission-name="${launch.mission_name}">
+                    <h2>${launch.mission_name}</h2>
+                    <p>${launch.launch_year}</p>
+                </article>
+            `;
+        });
+
+        $main.html(html);
+    }
+});
